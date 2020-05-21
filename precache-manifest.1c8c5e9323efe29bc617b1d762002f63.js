ba0012b44d8ca415bf39ddbc1fd2ca1bbce66f0e
sambreen27 <saba.ambreen16@gmail.com>
2020-05-21 01:42:30 -0400

    Updates

---
+++ precache-manifest.1c8c5e9323efe29bc617b1d762002f63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9700d269e81a906d0dac9fe649ae57aa",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "0adc523a1004eabbcc03",
+    "url": "/covid19/static/css/main.bd73ac2d.chunk.css"
+  },
+  {
+    "revision": "1ac24ab79e169b195a14",
+    "url": "/covid19/static/js/2.375f183d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.375f183d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adc523a1004eabbcc03",
+    "url": "/covid19/static/js/main.428d4cad.chunk.js"
+  },
+  {
+    "revision": "cdfcea41dfab553d9d0f",
+    "url": "/covid19/static/js/runtime-main.9f6017dd.js"
+  },
+  {
+    "revision": "7320530196dfb2c111dcadea1597d935",
+    "url": "/covid19/static/media/image.73205301.png"
+  }
+]);
